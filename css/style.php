f7216da40546476dc8587dae0a6d83786c47843a
Rakesh Mistry <mistryrn@mcmaster.ca>
2014-01-22 09:24:57 -0500

    Updated to be a template
    Tweaked everything, commented code with instructions, inserted generic
    icons, photos, and backgrounds to make this placeholder a template.

--- css/style.php
+++ css/style.php
@@ -41,7 +41,7 @@ html, body{
     display:block;position:absolute;z-index:-1;
     min-width:100%;
     min-height:100%;
-    background:transparent url('../images/bg<?php echo rand(1,5);?>.jpg') center center no-repeat;
+    background:transparent url('../images/bg<?php echo rand(1,2);?>.png') center center no-repeat;
     background-size:cover;
 }
 #profile { height: 325px; width: 325px; }
